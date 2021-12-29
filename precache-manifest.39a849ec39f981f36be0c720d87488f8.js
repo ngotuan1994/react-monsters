f1c0cdc22b54b7e4c44e27ed2789891f14a764ec
Tuan Ngo <ngotuan911@gmail.com>
2021-12-29 13:50:54 -0800

    Updates

---
+++ precache-manifest.39a849ec39f981f36be0c720d87488f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dac73234e0f16ba12584",
+    "url": "/react-monsters/static/css/main.3d92384c.chunk.css"
+  },
+  {
+    "revision": "dac73234e0f16ba12584",
+    "url": "/react-monsters/static/js/main.dac73234.chunk.js"
+  },
+  {
+    "revision": "8ee15efa1a2054ecf258",
+    "url": "/react-monsters/static/js/1.8ee15efa.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/react-monsters/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "37697f415408f47b2771",
+    "url": "/react-monsters/static/js/runtime~main.37697f41.js"
+  },
+  {
+    "revision": "9907a18ec288752346a586d561949704",
+    "url": "/react-monsters/index.html"
+  }
+];
